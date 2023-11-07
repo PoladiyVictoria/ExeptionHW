7ef596ab0add90bcc7b2bb1a8f03604ebc436bcb
PoladiyVictoria <vikapoladiy@gmail.com>
2023-11-07 15:57:03 +0300

    Созданы класс file для записи в новый файл

---
+++ data/file.java
@@ -0,0 +1,32 @@
+package data;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class file {
+    public void recordInFile(String[] contact){
+        String fileName = contact[0];
+        FileWriter fileWriter = null;
+        String cont = contact.toString();
+        try {
+           fileWriter = new FileWriter(fileName.toLowerCase() + ".txt");
+           fileWriter.write(cont);
+           
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                fileWriter.close();
+            }
+            catch (Exception e){
+
+            }
+
+        }
+    }
+}
